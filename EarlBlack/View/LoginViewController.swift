4f32bd818c846bea849cf96cc8c62ff82e2ced28
Bugra Muaz Mujde <mujdebugra@gmail.com>
2022-06-06 17:39:48 +0300

    Container View iptal
    Container View'in  hem dinamikleştiremediğimden hem de henüz children mantığını öğrenmediğimden daha sonraya erteledim.

--- EarlBlack/View/LoginViewController.swift
+++ EarlBlack/View/LoginViewController.swift
@@ -8,44 +8,24 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-
-    @IBOutlet weak var form_view: UIView!
     
+    @IBOutlet weak var email_text_field: UITextField!
+    
+    @IBOutlet weak var password_text_field: UITextField!
+    
+    @IBAction func sign_up_button(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let sign_up_view = storyboard.instantiateViewController(withIdentifier: "sign_up_storyboard_id") as! SignUpViewController
+        navigationController?.pushViewController(sign_up_view, animated: true)
+    }
+    
+    @IBAction func login_button(_ sender: Any) {
+        print(email_text_field.text)
+        print(password_text_field.text)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let controller = storyboard!.instantiateViewController(withIdentifier: "sayfa2")
-        self.add(controller, in: form_view)
     }
 }
 
-
-extension UIViewController {
-    func add(_ child: UIViewController, in container: UIView) {
-        addChild(child)
-        child.view.frame = container.bounds
-        container.addSubview(child.view)
-        
-        child.didMove(toParent: self)
-    }
-    
-    func add(_ child: UIViewController) {
-        add(child, in: view)
-    }
-    
-    func remove(from view: UIView) {
-        guard parent != nil else {
-            return
-        }
-        
-        willMove(toParent: nil)
-        removeFromParent()
-        view.removeFromSuperview()
-    }
-    
-    func remove() {
-        remove(from: view)
-    }
-}
---
+++ EarlBlack/View/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  EarlBlack
+//
+//  Created by queque on 5.06.2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("merasdasfas")
+    }
+}
