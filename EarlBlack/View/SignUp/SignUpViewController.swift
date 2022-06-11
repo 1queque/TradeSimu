c991c55d776ff297b747bf2c8da422b7f0832a9d
Bugra Muaz Mujde <mujdebugra@gmail.com>
2022-06-11 17:09:04 +0300

    Breath break

--- EarlBlack/View/SignUp/SignUpViewController.swift
+++ EarlBlack/View/SignUp/SignUpViewController.swift
@@ -9,28 +9,16 @@ import UIKit
 
 enum ErrorMessage: String{
     case fill_the_form = "Fill in all fields!"
-    case fill_all_red_fields = "Fill in all red fields!"
 }
 
 class SignUpViewController: UIViewController{
 
     @IBOutlet weak var sign_up_table_view: UITableView!
-    let user_coding_keys_array = User.add_all_coding_keys_to_array()
     var sign_up_form_dictionary: [String:UITextField] = [:]
     let sign_up_view_model = SignUpViewModel()
 
     @IBAction func submit_sign_up_table_view_through_done_button(_ sender: Any) {
-        if sign_up_form_dictionary.count == user_coding_keys_array.count{
-            if check_sign_up_form() {
-                var form_dictionary:[String:String] = [:]
-                for (key,value) in sign_up_form_dictionary{ form_dictionary[key] = value.text }
-                sign_up_view_model.set_data_to_encode(from: form_dictionary)
-            } else {
-                show_alert_message(for: ErrorMessage.fill_all_red_fields.rawValue)
-            }
-        } else {
-            show_alert_message(for: ErrorMessage.fill_the_form.rawValue)
-        }
+        sign_up_view_model.check_sign_up_form(from: sign_up_form_dictionary) ? () : (show_alert_message(for: ErrorMessage.fill_the_form.rawValue))
     }
 
     override func viewDidLoad() {
@@ -53,20 +41,6 @@ extension SignUpViewController{
         sign_up_table_view.register(UINib(nibName: "SignUpTableViewCell", bundle: nil), forCellReuseIdentifier: "sign_up_cell")
     }
     
-    func check_sign_up_form() -> Bool{
-        var is_filled:Bool = true
-        for (key, value) in sign_up_form_dictionary{
-            if value.text == ""{
-                is_filled = false
-                value.attributedPlaceholder = NSAttributedString(
-                    string: key.capitalize_first_letter_and_replace_underscore(),
-                    attributes: [NSAttributedString.Key.foregroundColor: UIColor.red]
-                )
-            }
-        }
-        return is_filled
-    }
-    
     func show_alert_message(for alert_message:String){
         let alert = UIAlertController(title: "", message: alert_message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
@@ -77,7 +51,7 @@ extension SignUpViewController{
 
 extension SignUpViewController:  UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return user_coding_keys_array.count
+        return sign_up_view_model.user_coding_keys_array.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -86,8 +60,12 @@ extension SignUpViewController:  UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "sign_up_cell", for: indexPath) as! SignUpTableViewCell
-        cell.title_label.text = user_coding_keys_array[indexPath.section]
-        cell.sign_up_text_field.placeholder = user_coding_keys_array[indexPath.section]
+        cell.title_label.text = sign_up_view_model.user_coding_keys_array[indexPath.section]
+        cell.sign_up_text_field.placeholder = sign_up_view_model.user_coding_keys_array[indexPath.section]
+        if cell.title_label.text == "Password"{
+            cell.sign_up_text_field.isSecureTextEntry = true
+            cell.sign_up_text_field.passwordRules = UITextInputPasswordRules(descriptor: "minlength: 8; required: lower; required: upper; required: digit; required: [!%*,.?_~];")
+        }
         sign_up_form_dictionary[cell.title_label.text?.lowercase_all_letter_and_replace_underscore() ?? ""] = cell.sign_up_text_field
         return cell
     }
--- EarlBlack/ViewModel/SignUpViewModel.swift
+++ EarlBlack/ViewModel/SignUpViewModel.swift
@@ -6,8 +6,24 @@
 //
 
 import Foundation
+import UIKit
 
 class SignUpViewModel{
+    
+    let user_coding_keys_array = User.add_all_coding_keys_to_array()
+    
+    func check_sign_up_form(from dict:[String:UITextField]) -> Bool{
+        if dict.count == user_coding_keys_array.count {
+            for (_, value) in dict{
+                if value.text == "" {
+                    return false
+                }
+            }
+        } else {
+            return false
+        }
+        return true
+    }
     
     func set_data_to_encode(from form_dictionary:[String:Any]){
         let user = User(from: form_dictionary)
